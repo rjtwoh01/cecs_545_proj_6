37cd62c338ddfb611f91b16f393ae11e92a2111d
rjtwoh01 <rjtwoh01@louisville.edu>
2018-11-11 13:24:17 -0500

    Working GUI

--- Nurikabe/Nurikabe/MainWindow.xaml.cs
+++ Nurikabe/Nurikabe/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Markup;
 
 namespace Nurikabe
 {
@@ -20,9 +22,75 @@ namespace Nurikabe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Viewbox viewbox;
+        private Canvas canvas;
+        Options options;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //string color = blocks[i, j].Center == false ? : "B" : "W";
+        public void InitializeBoard(BlockStruct[,] blocks, int n)
+        {
+            mainGrid.ShowGridLines = true;
+            TextBlock[,] block = new TextBlock[n,n];
+            mainGrid.ColumnDefinitions.Clear();
+            mainGrid.RowDefinitions.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                ColumnDefinition col = new ColumnDefinition();
+
+                mainGrid.ColumnDefinitions.Add(col);
+                mainGrid.RowDefinitions.Add(row);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < n; j++)
+                {
+                    string color = "W";
+                    if (blocks[i,j].Center == false)
+                    {
+                        color = "B";
+                    }
+                    else { color = ""; }
+                    var border = new Border
+                    {
+                        BorderBrush = Brushes.Black,
+                        BorderThickness = new Thickness(0.1, 0.1, 0.1, 0.1)
+                    };
+                    block[i, j] = new TextBlock
+                    {
+                        FontSize = 15,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Text = color
+                    };
+                    border.Child = block[i, j];
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+                    mainGrid.Children.Add(border);
+                }
+            }
+        }
+
+        private void btnOptions_Click(object sender, RoutedEventArgs e)
+        {
+            //mainGrid.Children.Clear();
+            if (this.options == null)
+            {
+                this.options = new Options(this);
+            }
+
+            this.options.Show();
+        }
+
+        private void ClearScreen()
+        {
+            mainGrid.Children.Clear();
         }
     }
 }
---
+++ Nurikabe/Nurikabe/Options.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Nurikabe
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        private MainWindow mainWindow;
+        private bool forceClose = false;
+
+        public Options(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!this.forceClose)
+            {
+                this.Hide();
+                e.Cancel = true;
+            }
+        }
+
+        public void ForceClose()
+        {
+            this.forceClose = true;
+            this.Close();
+        }
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            BlockStruct[,] blocks = InitializationHelper.InitializeBlockArray(Convert.ToInt32(txtBoxSize.Text));
+            int i = 0;
+            foreach (BlockStruct block in blocks)
+            {
+                string color = block.Center == false ? "black" : "white";
+                Debug.WriteLine(i + ": " + color + "(" + block.Center + ")");
+                i++;
+            }
+
+            mainWindow.InitializeBoard(blocks, Convert.ToInt32(txtBoxSize.Text));
+            this.Hide();
+        }
+    }
+}
